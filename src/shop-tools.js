c33f7fceeeb61f26608c3b1c2ac423c9bb30189f
dany corbineau <danycor2@hotmail.fr>
2017-11-21 15:24:20 +0100

    exercice 2 fini et fonctionne

--- src/shop-tools.js
+++ src/shop-tools.js
@@ -2,3 +2,8 @@ exports.getPrices=function getPrices()
 {
     return { banana: 1, potato: 2, tomato: 3, cucumber: 4, salad: 5, apple: 6 };
 }
+
+exports.countArticles=function countArticles()
+{
+    return [ 'tomato', 'cucumber', 'tomato', 'salad', 'potato', 'cucumber', 'potato', 'potato', 'tomato', 'potato' ].length;
+}
--- test/shop-tools.test.js
+++ test/shop-tools.test.js
@@ -3,7 +3,12 @@ var lib1=require('../src/shop-tools')
 
 var expect = require('chai').expect;
     describe('test assertion', function() {
-    it('insert Correct?', function() {
+    it('insert Correct (object)?', function() {
         expect({banana:1, potato: 2, tomato: 3, cucumber: 4, salad: 5, apple: 6 }).to.eql(lib1.getPrices());
     })
+
+    it('insert 2 Correct (count)?', function() {
+        expect( [ 'tomato', 'cucumber', 'tomato', 'salad', 'potato', 'cucumber', 'potato', 'potato', 'tomato', 'potato' ].length).to.eql(lib1.countArticles());
+    })
+
 });
